ee6142b3633ba169505b11944170692ccde9a4cd
DianaSemenova <menestrelia199702@gmail.com>
2026-03-10 13:45:52 +0400

    feat: Initialize project with build

---
+++ eslint.config.mjs
@@ -0,0 +1,56 @@
+import eslintJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import eslintReact from "eslint-plugin-react";
+import eslintReactHooks from "eslint-plugin-react-hooks";
+import eslintPrettier from "eslint-plugin-prettier";
+import eslintPrettierConfig from "eslint-config-prettier";
+import eslintImport from "eslint-plugin-import";
+
+const tsConfigPath = "./tsconfig.json";
+
+export default [
+  eslintJs.configs.recommended,
+  eslintImport.flatConfigs.recommended,
+  eslintImport.flatConfigs.typescript,
+  ...tseslint.configs.recommended,
+  {
+    languageOptions: {
+      parserOptions: {
+        tsConfigPath,
+        ecmaVersion: 2020,
+        browser: true,
+        node: true,
+        // tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    settings: {
+      react: { version: "detect" },
+      "import/resolver": {
+        typescript: {
+          alwaysTryTypes: true,
+          tsConfigPath,
+        },
+      },
+    },
+    plugins: {
+      react: eslintReact,
+      "react-hooks": eslintReactHooks,
+      prettier: eslintPrettier,
+    },
+    rules: {
+      ...eslintReactHooks.configs.recommended.rules,
+      "prettier/prettier": ["error"],
+      "@typescript-eslint/no-explicit-any": "off",
+      "no-console": ["warn", { allow: ["warn", "error"] }],
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        { argsIgnorePattern: "^_" },
+      ],
+    },
+  },
+  {
+    // Что игнорируем
+    ignores: ["dist", "node_modules", "vite.config.ts"],
+  },
+  eslintPrettierConfig, // Должен быть в конце, чтобы отключить конфликтующие правила
+];
---
+++ prettier.config.mjs
@@ -0,0 +1,10 @@
+/**
+ * @type {import("prettier").Config}
+ */
+export default {
+  singleQuote: false,
+  trailingComma: "es5",
+  bracketSpacing: true,
+  semi: true,
+  endOfLine: "auto",
+};
