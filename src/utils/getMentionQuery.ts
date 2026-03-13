908d35479690b3cae3922f6c11b2ebc2f5b89027
DianaSemenova <menestrelia199702@gmail.com>
2026-03-13 17:42:34 +0400

    fix getMentionQuery

--- src/utils/getMentionQuery.ts
+++ src/utils/getMentionQuery.ts
@@ -1,8 +1,15 @@
 export const getMentionQuery = (text: string, cursorPosition: number) => {
+  const allowedBeforeAt = ['', ' ', '.', ',', '!', '?', ';', ':', '\n'];
+
   const textBeforeCursor = text.slice(0, cursorPosition);
   const lastAtPos = textBeforeCursor.lastIndexOf('@');
 
-  if (lastAtPos !== -1 && !textBeforeCursor.slice(lastAtPos).includes(' ')) {
+  if (lastAtPos === -1 || textBeforeCursor.slice(lastAtPos).includes(' ')) {
+    return { query: '', isMentioning: false, atIndex: -1 };
+  }
+  const charBeforeAt = lastAtPos > 0 ? textBeforeCursor[lastAtPos - 1] : '';
+
+  if (allowedBeforeAt.includes(charBeforeAt)) {
     return {
       query: textBeforeCursor.slice(lastAtPos + 1),
       isMentioning: true,
